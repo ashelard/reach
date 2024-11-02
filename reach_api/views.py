b1b02d286b037dc49d0381ecf9b0ba87ad18a68a
unknown <zilong_10@126.com>
2024-11-02 17:59:30 +0800

    tst

--- reach_api/views.py
+++ reach_api/views.py
@@ -6,15 +6,15 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Counters
 
-
 logger = logging.getLogger('log')
 
 
-def get_current_time(request):
+def get_current_time(request, _):
     now = datetime.now()
 
     # 按照指定格式打印当前日期和时间
     time = now.strftime("%Y-%m-%d %H:%M:%S")
+    logger.info('get current time result: {}'.format(time))
     return JsonResponse({'code': 0, 'data': time},
                         json_dumps_params={'ensure_ascii': False})
 
@@ -43,7 +43,7 @@ def counter(request, _):
         rsp = update_count(request)
     else:
         rsp = JsonResponse({'code': -1, 'errorMsg': '请求方式错误'},
-                            json_dumps_params={'ensure_ascii': False})
+                           json_dumps_params={'ensure_ascii': False})
     logger.info('response result: {}'.format(rsp.content.decode('utf-8')))
     return rsp
 
@@ -57,7 +57,7 @@ def get_count():
         data = Counters.objects.get(id=1)
     except Counters.DoesNotExist:
         return JsonResponse({'code': 0, 'data': 0},
-                    json_dumps_params={'ensure_ascii': False})
+                            json_dumps_params={'ensure_ascii': False})
     return JsonResponse({'code': 0, 'data': data.count},
                         json_dumps_params={'ensure_ascii': False})
 
@@ -87,7 +87,7 @@ def update_count(request):
         data.count += 1
         data.save()
         return JsonResponse({'code': 0, "data": data.count},
-                    json_dumps_params={'ensure_ascii': False})
+                            json_dumps_params={'ensure_ascii': False})
     elif body['action'] == 'clear':
         try:
             data = Counters.objects.get(id=1)
@@ -95,7 +95,7 @@ def update_count(request):
         except Counters.DoesNotExist:
             logger.info('record not exist')
         return JsonResponse({'code': 0, 'data': 0},
-                    json_dumps_params={'ensure_ascii': False})
+                            json_dumps_params={'ensure_ascii': False})
     else:
         return JsonResponse({'code': -1, 'errorMsg': 'action参数错误'},
-                    json_dumps_params={'ensure_ascii': False})
+                            json_dumps_params={'ensure_ascii': False})
