07c27d356574c17b9616a6a44a20864259be61d0
unknown <zilong_10@126.com>
2024-11-05 16:45:17 +0800

    fix

--- wb_spider/wspider/settings.py
+++ wb_spider/wspider/settings.py
@@ -12,11 +12,8 @@ NEWSPIDER_MODULE = 'spiders'
 
 ROBOTSTXT_OBEY = False
 
-with open('cookie.txt', 'rt', encoding='utf-8') as f:
-    cookie = f.read().strip()
 DEFAULT_REQUEST_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/61.0',
-    'Cookie': cookie
 }
 
 CONCURRENT_REQUESTS = 16
