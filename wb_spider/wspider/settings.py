5e7e1848f205294e1b99376542dd4461b3f264e9
unknown <zilong_10@126.com>
2024-11-05 21:16:24 +0800

    debug

--- wb_spider/wspider/settings.py
+++ wb_spider/wspider/settings.py
@@ -16,7 +16,7 @@ DEFAULT_REQUEST_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/61.0',
 }
 
-LOG_LEVEL = 'INFO'
+LOG_LEVEL = 'DEBUG'
 
 CONCURRENT_REQUESTS = 16
 
