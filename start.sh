987a16c88238929e2b7d58c2dda1f0f9bb5d8e7d
unknown <zilong_10@126.com>
2024-11-06 17:53:58 +0800

    launch scrapyd

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#set -e
-#scrapyd
-#
-#cd /app/wb_spider
-#scrapyd-deploy
+set -e
+scrapyd &
+
+cd /app/wb_spider
+scrapyd-deploy
 
 python3 manage.py runserver 0.0.0.0:80
 echo "start runserver end" >> logs/info-2024-11-06.log
