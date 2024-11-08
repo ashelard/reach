2717c25b119d1c0e4912187e98bc5b3fbc5b647a
unknown <zilong_10@126.com>
2024-11-08 15:39:16 +0800

    test signal

--- reach_api/apps.py
+++ reach_api/apps.py
@@ -3,8 +3,6 @@ import os
 
 from django.apps import AppConfig
 
-from reach_api.schedule.scheduler import start_scheduler
-
 
 class AppNameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -13,12 +11,4 @@ class AppNameConfig(AppConfig):
     ready_run = False
 
     def ready(self):
-        if AppNameConfig.ready_run:
-            return
-        try:
-            self.logger.info(f"Ready method called.PID:{os.getpid()}")
-        except Exception as e:
-            self.logger.error("Error in ready method: %s", e)
-
-        start_scheduler()
-        AppNameConfig.ready_run = True
+        import reach_api.schedule.scheduler
--- reach_api/schedule/scheduler.py
+++ reach_api/schedule/scheduler.py
@@ -1,6 +1,8 @@
 import logging
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 log = logging.getLogger('log')
 
@@ -8,8 +10,10 @@ def job():
     log.info("----------------------------Job started-------------------------------")
 
 
-def start_scheduler():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(job, 'interval', seconds=30)
-    scheduler.start()
-    log.info("---------scheduler runned---------------")
+@receiver(post_migrate)
+def start_scheduler(sender, **kwargs):
+    if sender.name == 'reach_api':
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(job, 'interval', seconds=30)
+        scheduler.start()
+        log.info("---------scheduler runned---------------")
