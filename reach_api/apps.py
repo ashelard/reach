522babf5243a4a89533bdd034edc87fe2f28af8a
unknown <zilong_10@126.com>
2024-11-08 14:04:33 +0800

    call ready

--- reach_api/apps.py
+++ reach_api/apps.py
@@ -9,12 +9,15 @@ class AppNameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reach_api'
     logger = logging.getLogger('log')
+    ready_run = False
 
     def ready(self):
+        if AppNameConfig.ready_run:
+            return
         try:
             self.logger.info("Ready method called.")
-            # 你的初始化代码
         except Exception as e:
             self.logger.error("Error in ready method: %s", e)
 
         start_scheduler()
+        AppNameConfig.ready_run = True
