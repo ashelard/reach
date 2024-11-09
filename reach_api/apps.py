bbff84e892537ce962ee7097d78b23df63104ae1
unknown <zilong_10@126.com>
2024-11-09 12:08:15 +0800

    remove task from django

--- reach_api/apps.py
+++ reach_api/apps.py
@@ -3,14 +3,10 @@ import os
 
 from django.apps import AppConfig
 
-import reach_api.schedule.scheduler
-
 
 class AppNameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reach_api'
-    logger = logging.getLogger('log')
-    ready_run = False
 
     def ready(self):
         pass
--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ cd /app/wb_spider
 scrapyd-deploy
 
 cd /app
-python3 manage.py start_task &
+#python3 manage.py start_task &
 python3 manage.py runserver 0.0.0.0:80
 
